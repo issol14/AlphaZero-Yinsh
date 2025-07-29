5a8366c1ea87e1c4ebc38a7d9676abc3148c8a0d
issol14 <0214leedasol@gmail.com>
2025-07-29 12:38:13 +0000

    dd

--- code/api/server.py
+++ code/api/server.py
@@ -68,7 +68,7 @@ app = FastAPI(
 # CORS 설정
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["localhost:3000", "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
